3d8ec1fc000bb485b24a3068d58f99ac68cf0cc5
Leo Leite <leonardotleite13k@gmail.com>
2021-11-16 18:16:00 -0300

    feat: added loading page to signIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import { ButtonPrimary, ButtonSecundary } from '../../components/Button';
-
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <div>
-      <h1>SignIn</h1>
-      <ButtonPrimary onClick={() => console.log('click')} text="Cadastrar" />
-      <ButtonSecundary onClick={() => console.log('click')} text="Entrar" />
-    </div>
+    <Container>
+      <h1>Em construção 🛠</h1>
+      {/* <ButtonPrimary onClick={() => console.log('click')} text="Cadastrar" />
+      <ButtonSecundary onClick={() => console.log('click')} text="Entrar" /> */}
+    </Container>
   );
 }
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  height: 100vh;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;

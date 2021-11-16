e530f37050fdd6733ded002b26838a883b71336f
Leo Leite <leonardotleite13k@gmail.com>
2021-11-16 08:25:19 -0300

    feat: added estructure and navigation

---
+++ src/pages/Track/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Track: React.FC = () => {
+  return <div />;
+}
+
+export default Track;
---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Avatar from '../pages/Avatar';
+import Home from '../pages/Home';
+import Track from '../pages/Track';
+import Journey from '../pages/Journey';
+import Ranking from '../pages/Ranking';
+
+import Route from './Route';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signUp" component={SignUp} />
+      <Route path="/avatar" component={Avatar} isPrivate />
+      <Route path="/home" component={Home} isPrivate />
+      <Route path="/track/:id" component={Track} isPrivate />
+      <Route path="/journey" component={Journey} isPrivate />
+      <Route path="/ranking" component={Ranking} isPrivate />
+    </Switch>
+  );
+};
+
+export default Routes;

e5817050d1d074614ef65d49216ae0d24af552fa
Leo Leite <leonardotleite13k@gmail.com>
2021-11-16 10:39:41 -0300

    feat: added prettier and editorconfig

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { IButton } from './interface';
 // import { Container } from './styles';
 
-const Button: React.FC = () => {
-  return <div />;
+const Button: React.FC<IButton> = ({ text }) => {
+  return (
+    <button>{text}</button>
+  );
 }
 
 export default Button;
